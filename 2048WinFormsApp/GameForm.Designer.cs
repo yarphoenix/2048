401c1f3de782d0d624970bf4b0be57347225c589
yarphoenix <yarikhectatuct@gmail.com>
2025-09-14 23:20:45 +0300

    Добавьте файлы проекта.

---
+++ 2048WinFormsApp/GameForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace _2048WinFormsApp
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // GameForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(439, 445);
+            Name = "GameForm";
+            Text = "GameForm";
+            Load += GameForm_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ 2048WinFormsApp/GameForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2048WinFormsApp
+{
+    public partial class GameForm : Form
+    {
+        private const int MapSize = 4;
+
+        private static readonly Random Random = new();
+
+        private Label[,] _labelsMap;
+
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+            InitMap();
+            GenerateNumber();
+        }
+
+        private void InitMap()
+        {
+            _labelsMap = new Label[MapSize, MapSize];
+
+            for (var i = 0; i < MapSize; i++)
+            {
+                for (var j = 0; j < MapSize; j++)
+                {
+                    var newLabel = CreateLabel(i, j);
+                    Controls.Add(newLabel);
+                    _labelsMap[i, j] = newLabel;
+                }
+            }
+        }
+        private void GenerateNumber()
+        {
+            int randomNumberLabel = Random.Next(MapSize * MapSize);
+            int row = randomNumberLabel / MapSize;
+            int column = randomNumberLabel % MapSize;
+            if (_labelsMap[row, column].Text == string.Empty)
+            {
+                // 2 или 4
+                _labelsMap[row, column].Text = "2";
+            }
+        }
+
+
+        private static Label CreateLabel(int row, int column)
+        {
+            var label = new Label();
+            label.BackColor = Color.Silver;
+            label.Font = new Font("Yu Gothic UI Semibold", 20F, FontStyle.Bold);
+            label.Size = new Size(100, 100);
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            int x = 10 + column * 107;
+            int y = 10 + row * 107;
+            label.Location = new Point(x, y);
+
+            return label;
+        }
+    }
+}
