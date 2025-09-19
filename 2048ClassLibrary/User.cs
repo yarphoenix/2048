dcee0b77d1c4051c87191e61793d2cf95b24e314
yarphoenix <yarikhectatuct@gmail.com>
2025-09-19 21:26:35 +0300

    Создан отдельный проект для вынесения общей логики игры.

---
+++ 2048ClassLibrary/User.cs
@@ -0,0 +1,12 @@
+﻿namespace _2048ClassLibrary;
+
+public class User(string name)
+{
+    private readonly string? _name = name;
+    private int _score;
+
+    public void ChangeScore(int value)
+    {
+        _score += value;
+    }
+}
